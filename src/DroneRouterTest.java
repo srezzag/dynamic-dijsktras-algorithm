81c82f6de8ff639cfa1d43c96bc59cce4e5fd7bb
Samy Rezzag <43894767+srezzag@users.noreply.github.com>
2019-07-18 00:06:19 -0500

    Update DroneRouterTest.java

--- src/DroneRouterTest.java
+++ src/DroneRouterTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-import edu.metrostate.ics340.p3.msr812.DroneRouter;
+import package.DroneRouter;
 
 class DroneRouterTest {
 
